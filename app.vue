e850a6420a85b23526d369f51a878f7f3b8943ce
Vincent Brouwer <dviide@protonmail.com>
2024-04-14 01:12:19 +0200

    maybe fix for animation

--- app.vue
+++ app.vue
@@ -37,20 +37,4 @@ useHead({
 
 <style lang="scss">
 @import url("/assets/scss/main.scss");
-
-html,
-body {
-  margin: 0;
-  padding: 0;
-}
-
-.page-enter-active,
-.page-leave-active {
-  transition: opacity 0.3s;
-  will-change: filter;
-}
-.page-enter-from,
-.page-leave-to {
-  opacity: 0;
-}
 </style>
